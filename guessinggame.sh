0b9e2241bc798f1db03c55daba3f3354e15f6b88
Jose Cappelletto <cappelletto@gmail.com>
2018-03-28 12:37:30 -0400

    Added function_count_files()

--- guessinggame.sh
+++ guessinggame.sh
@@ -4,28 +4,36 @@
 # Available from https://githup.com/cappelletto/ TODO
 # Cousera. Course "Unix Workbench - Week 4 - Project"
 
-echo 'Test'
+function_count_files()
+{
+	N_FILES=$(ls -1 | wc -l)
+}
+
 # First, we must retrieve the number of files (N_FILES) in the current folder.
 # We choose to ignore subfolders, and hidden files/folders when retrieving the list with 'ls'
 
-N_FILES=$(ls -1 | wc -l)
+# Retrieve the number of files in the current working directory using a function
+#N_FILES=$( function_count_files )
 
-echo $N_FILES
+N_FILES=0
+function_count_files
+
 # Then, we ask the user for the input (USER_GUESS), until it guess correctly. If USER_GUESS != N_FILES
 # we inform if higher or lower, and ask again 
 
 read -p "Guess the number of files in the current directory: " USER_GUESS
 
-N_FILES=5
-
+# While doesn't guess correctly
 while [ $USER_GUESS -ne $N_FILES ]
 	do
+		# Check if lower than..
 		if [ $USER_GUESS -lt $N_FILES ]
 		then
 			read -p "Guess too low, try a with a higher number: " USER_GUESS
-		else 
+		else # is higher
 			read -p "Guess too high, try a with a lower number: " USER_GUESS
 		fi
 	done
 
 echo "Congratulations!! you guessed right, there are "$N_FILES" files in this folder"
+
