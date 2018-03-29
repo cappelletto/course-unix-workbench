c709c1ed42f8750aa7665510c366051bf83e6d3d
Jose Cappelletto <cappelletto@gmail.com>
2018-03-29 15:28:50 -0400

    Added alternative to line count, to skip those starting by #

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,33 +1,24 @@
 #!/bin/bash
-
 # Created by J.Cappelletto (cappelletto [at] gmail [dot] com
-# Available from https://githup.com/cappelletto/ TODO
-# Cousera. Course "Unix Workbench - Week 4 - Project"
+# Available from https://github.com/cappelletto/
+# Cousera: "Unix Workbench - Week 4. Final Project"
 
-function_count_files()
+function_count_files()	# Mandatory presence of function
 {
 	N_FILES=$(ls -1 | wc -l)
 }
 
-# First, we must retrieve the number of files (N_FILES) in the current folder.
-# We choose to ignore subfolders, and hidden files/folders when retrieving the list with 'ls'
-
+# First, we must retrieve the number of files (N_FILES) in the current folder. Ignore subfolders, and hidden files/folders
 # Retrieve the number of files in the current working directory using a function
-#N_FILES=$( function_count_files )
-
-N_FILES=0
 function_count_files
 
 # Then, we ask the user for the input (USER_GUESS), until it guess correctly. If USER_GUESS != N_FILES
-# we inform if higher or lower, and ask again 
 
 read -p "Guess the number of files in the current directory: " USER_GUESS
 
-# While doesn't guess correctly
-while [ $USER_GUESS -ne $N_FILES ]
+while [ $USER_GUESS -ne $N_FILES ] # While doesn't guess correctly
 	do
-		# Check if lower than..
-		if [ $USER_GUESS -lt $N_FILES ]
+		if [ $USER_GUESS -lt $N_FILES ]	# Check if lower than..
 		then
 			read -p "Guess too low, try a with a higher number: " USER_GUESS
 		else # is higher
--- makefile
+++ makefile
@@ -4,14 +4,15 @@
 #    The date and time at which make was run.
 #    The number of lines of code contained in guessinggame.sh.
 
-#The README.md should be produced entirely from the makefile and should not be edited by hand.
+# The README.md should be produced entirely from the makefile and should not be edited by hand.
 
 # As the README.md must updated always (date+time will change), then we must overwrite it
 # Project title: The Unix Workbench - Final Project. 
 
 # We create a make rule for README.md
-README.md:
-	echo "Coursera: The Unix Workbench - Final Project. (JC)" > README.md
+README.md:	guessinggame.sh
+	echo "The Unix Workbench - Final Project. (JC)" > README.md
 	date >> README.md
 	wc -l guessinggame.sh | egrep -o "[0-9]+" >> README.md 
-
+# Or this more specific version, which doesn't count lines that start with comment
+#    cat guessinggame.sh | egrep -v "^\#" | wc -l >> README.md
